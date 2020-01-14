2a55731d8c8033309f32953e4528cd389224f36b
Kushagra Agarwal <agarwal.kush17181@gmail.com>
2020-01-14 10:39:22 +0530

    Completed authrisation

--- auth.js
+++ auth.js
@@ -1,50 +1,55 @@
-module.exports.auth = function (){
-    console.log('doing auth');
-    consumerKey = "PifhPHSIB4yUXGuBe5oWLKrjg";
-    consumerSecret = "H80Txqsu0fu9uelcBssATuvFOHh9vXFN4Gz8FkrueUsEDnGoBb";
+const Configstore = require('configstore');
+const packageJson = require('./package.json');
+const conf = new Configstore(packageJson.name);
 
-    var OAuth = require('oauth');
-    var oauth = new OAuth.OAuth(
-        'https://api.twitter.com/oauth/request_token',
-        'https://api.twitter.com/oauth/access_token',
-        consumerKey,
-        consumerSecret,
-        '1.0A',
-        'oob',
-        'HMAC-SHA1'
-    );
-    oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
-        if(error) console.log(error);
-        else{
-            console.log('oauth_token :' + oauth_token);
-            console.log('oauth_token_secret :' + oauth_token_secret);
-            console.log('requestoken results :' + JSON.stringify(results));
-            console.log("Requesting access token");
+module.exports = {
+    getCredentials :() =>{
+        consumerKey = "PifhPHSIB4yUXGuBe5oWLKrjg";
+        consumerSecret = "H80Txqsu0fu9uelcBssATuvFOHh9vXFN4Gz8FkrueUsEDnGoBb";
+        var OAuth = require('oauth');
+        var oauth = new OAuth.OAuth(
+            'https://api.twitter.com/oauth/request_token',
+            'https://api.twitter.com/oauth/access_token',
+            consumerKey,
+            consumerSecret,
+            '1.0A',
+            'oob',
+            'HMAC-SHA1'
+        );
+        oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+            if(error) console.log(error);
+            else{
+                console.log('oauth_token :' + oauth_token);
+                console.log('oauth_token_secret :' + oauth_token_secret);
+                console.log('requestoken results :' + JSON.stringify(results));
+                console.log("Requesting access token");
 
-            console.log('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token + "\n");
-            var stdin = process.openStdin();
-            console.log('PIN: ');
-            stdin.on('data', function(d) {
-                d = (d+'').trim();
-                if(!d) {
-                    console.warn('\nTry again: ');
-                }
-                console.log('Received PIN: ' + d);
-                oauth.getOAuthAccessToken(oauth_token, oauth_token_secret, d, function(err, oauth_access_token, oauth_access_token_secret, results2) {
-                    if(err) throw err;
-    
-                    console.log(results2);
-    
-                    oauth.accessToken = oauth_access_token;
-                    oauth.accessTokenSecret = oauth_access_token_secret;
-    
-                    /*oauth.getProtectedResource("https://api.twitter.com/1/statuses/home_timeline.json", "GET", oauth_access_token, oauth_access_token_secret, function (error, data, response) {
-                        console.log(data);
-                    });*/
-                    
-                    stdin.destroySoon();
+                console.log('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token + "\n");
+                var stdin = process.openStdin();
+                console.log('PIN: ');
+                stdin.on('data', function(d) {
+                    d = (d+'').trim();
+                    if(!d) {
+                        console.warn('\nTry again: ');
+                    }
+                    console.log('Received PIN: ' + d);
+                    oauth.getOAuthAccessToken(oauth_token, oauth_token_secret, d, function(err, oauth_access_token, oauth_access_token_secret, results2) {
+                        if(err) throw err;
+
+                        console.log(results2);
+                        
+                        
+                        oauth.accessToken = oauth_access_token;
+                        oauth.accessTokenSecret = oauth_access_token_secret;
+                        
+                        stdin.destroySoon();
+
+                        conf.set('done',true);
+                        conf.set('token.at',oauth_access_token);
+                        conf.set('token.ats',oauth_access_token_secret);
+                    });
                 });
-            });
-        }
-    })
+            }
+        })
+    }
 }
--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
 
 const auth = require('./auth.js');
-auth.auth();
+const Configstore = require('configstore');
+const packageJson = require('./package.json');
+const conf = new Configstore(packageJson.name);
+
+console.log('doing auth');
+var tokenDone = conf.get('done');
+if(!tokenDone){
+    auth.getCredentials();
+}
+else{
+    var token = conf.get('token');
+    console.log(token);
+}
